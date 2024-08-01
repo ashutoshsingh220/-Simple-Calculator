d0e05f3cd33c089907e1cadec8ca5585eff492f2
ashutoshsingh220 <ashutosh220kharayat@gmail.com>
2024-08-01 19:35:19 +0530

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ float a,b,add,sub,mult,div;
+ cout<<"enter first no"<<endl;
+ cin>>a;
+ cout<<"enter second number"<<endl;
+ cin>>b;
+ cout<<"displaying results"<<endl;
+ add=a+b;
+ sub=a-b;
+ mult=a*b;
+ div=a/b;
+//jodana other name of addition.
+ cout<<"jodana-"<<add<<endl;
+//ghataav other name of subtraction.
+ cout<<"ghataav-"<<sub<<endl;
+//guna other name of multiplication.
+ cout<<"guna-"<<mult<<endl;
+//vibhaajan other name of division.
+ cout<<"vibhaajan-"<<div<<endl;
+ return 0;
+}
+
+//output
+/*enter first no
+24
+enter second number
+10
+displaying results
+jodana-34
+ghataav-14
+guna-240
+vibhaajan-2.4*/
